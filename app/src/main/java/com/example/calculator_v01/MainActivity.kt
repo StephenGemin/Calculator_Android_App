8b4fbbfa34fe1477d9f768faa49406ab0868aa7a
StephenGemin <45926479+StephenGemin@users.noreply.github.com>
2019-06-21 21:00:42 -0400

    Adding percent and sign change functions
    Additions:
    1. onPercent
    2. onSign
    3. separate prive function that takes care of evaluation.  Allows me to call the same function within the percent and equal buttons.

--- app/src/main/java/com/example/calculator_v01/MainActivity.kt
+++ app/src/main/java/com/example/calculator_v01/MainActivity.kt
@@ -1,22 +1,29 @@
 package com.example.calculator_v01
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import net.objecthunter.exp4j.ExpressionBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
 import java.lang.ArithmeticException
 
+private const val TAG = "CALC"
+
 class MainActivity : AppCompatActivity() {
+
+
 
     lateinit var calcOutput : TextView
     lateinit var calcHist : TextView
     var buildString = ArrayList<String>()
     var lastNumeric = false
     var lastEqual = false
+    var lastPercent = false
     var errorState = false
     var onlyDec = false
 
@@ -47,13 +54,11 @@ class MainActivity : AppCompatActivity() {
             onlyDec = true
         }
     }
-
     private fun buildTextOutput():String{
         var txtOut = ""
         for (item in buildString){
             txtOut += item
         }
-        Toast.makeText(this, "expression: $txtOut", Toast.LENGTH_SHORT).show()
         return txtOut
     }
 
@@ -67,7 +72,7 @@ class MainActivity : AppCompatActivity() {
             this.calcOutput.text = ""
             calcHist.text = txt
             lastNumeric = false
-            onlyDec = false     //reset the decimal flag
+            onlyDec = false
         }
     }
 
@@ -80,25 +85,64 @@ class MainActivity : AppCompatActivity() {
         this.buildString = ArrayList()
     }
 
+    fun onSign(view:View){
+        if (calcOutput.text.isBlank()){
+            // don't do anything
+        }else if (calcOutput.text.isNotBlank()){
+            calcOutput.text = (toDbl(calcOutput) * -1).toString()
+        }
+
+    }
+
+    fun onPercent(view:View){
+        if (calcOutput.text.isBlank() && buildString.isEmpty() || calcOutput.text.toString().toDoubleOrNull() == null){
+            // don't do anything
+        }else if (buildString.isEmpty() && calcOutput.text.isNotBlank()){
+            calcOutput.text = (toDbl(calcOutput) / 100).toString()
+            lastPercent = true
+        }else if (buildString.isNotEmpty() && calcOutput.text.isNotBlank()){
+            calcOutput.text = evalPercent(calcOutput.text.toString()).toString()
+//            Log.i(TAG, "Percent function -> buildString: $buildString")
+//            Log.i(TAG, "Percent function -> calcOutput: ${calcOutput.text}")
+        }
+    }
+
+    private fun evalPercent(userInput:String):Double{
+        return eval(buildTextOutput() + "0").toDouble() * (userInput.toDouble() / 100)
+    }
+
     fun onEqual(view:View){
         var txt:String
-        if (lastNumeric && !errorState){
+        if (lastNumeric && !errorState && buildString.isNotEmpty()){
             txt = buildTextOutput()
             txt += calcOutput.text.toString()
-            Toast.makeText(this, "expression: $txt", Toast.LENGTH_SHORT).show()
-            val expression = ExpressionBuilder(txt).build()
             try{
-                val result = expression.evaluate()
-                calcOutput.text = result.toString()
+                calcOutput.text = eval(txt)
                 onlyDec = true
             }catch (ex: ArithmeticException){
                 calcOutput.text = getString(R.string.error)
                 errorState = true
                 lastNumeric = false
-
             }
         lastEqual = true
         this.buildString = ArrayList()
         }
+    }
+
+    private fun eval(txt:String):String{
+        Log.i(TAG, "eval method -> String to evaluate: $txt")
+        val expression = ExpressionBuilder(txt).build()
+        return (expression.evaluate()).toString()
+    }
+
+//    private fun dblToInt(num:Double):String{
+//        if (num - num.toInt() == 0.0){
+//            return num.toInt().toString()
+//        }
+//        return num.toString()
+//    }
+
+    private fun toDbl(input:TextView):Double{
+        return input.text.toString().toDouble()
     }
 }
